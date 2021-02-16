3e654b14781cf891d4cb996b7811d7226e6d85f7
songjw3383 <56250064+songjw3383@users.noreply.github.com>
2021-02-17 00:05:31 +0900

    Finished

--- vanilla-redux/src/components/App.js
+++ vanilla-redux/src/components/App.js
@@ -3,14 +3,13 @@ import { HashRouter as Router, Route } from "react-router-dom";
 import Home from "../routes/Home";
 import Detail from "../routes/Detail";
 
-
-function App(){
-    return ( 
+function App() {
+  return (
     <Router>
-        <Route path="/" exact component={Home}></Route>
-        <Route path="/:id" component={Detail}></Route>
+      <Route path="/" exact component={Home}></Route>
+      <Route path="/:id" component={Detail}></Route>
     </Router>
-    );
+  );
 }
 
 export default App;
--- vanilla-redux/src/components/ToDo.js
+++ vanilla-redux/src/components/ToDo.js
@@ -1,21 +1,21 @@
 import React from "react"
 import { connect } from "react-redux";
-import { actionCreators } from "../store";
+import { remove } from "../store";
 import { Link } from "react-router-dom";
 
 function ToDo({text, onBtnClick, id}) {
     return ( 
     <li>
         <Link to={`/${id}`}>
-        {text} <button onClick={onBtnClick}>DEL</button>
         </Link>
+        {text} <button onClick={onBtnClick}>DEL</button>
     </li>
     );
 }
 
 function mapDispatchToProps(dispatch, ownProps){
     return {
-        onBtnClick: () => dispatch(actionCreators.deleteToDo(ownProps.id))
+        onBtnClick: () => dispatch(remove(ownProps.id))
     }
 }
 
